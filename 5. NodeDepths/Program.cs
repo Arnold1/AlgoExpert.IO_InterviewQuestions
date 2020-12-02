2f34d6b1163ac9c13ecbb32df3ed924c9250c7bd
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-02 12:20:58 +0000

    Question 5 - Node Depths Complete - 2 solutions

---
+++ 5. NodeDepths/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _5._NodeDepths
+{
+    public class BinaryTree
+    {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value)
+        {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree tree = CreateTree();
+
+            Console.WriteLine($"(Iterative Method) The node depth is {IterativeMethod(tree)}");
+            Console.WriteLine($"(Recursive Method) The node depth is {RecursiveMethod(tree)}");
+            Console.WriteLine();
+        }
+
+        public static int RecursiveMethod(BinaryTree tree, int depth=0)
+        {
+            // Time - O(N) where N is the number of nodes in tree
+            // Space - O(H) where H is the height of tree
+            if(tree == null)
+            {
+                return 0;
+            }
+            return depth + RecursiveMethod(tree.left, depth + 1) + RecursiveMethod(tree.right, depth + 1);
+        }
+
+        public static int IterativeMethod(BinaryTree tree)
+        {
+            // Time - O(N) where N is the number of nodes in tree
+            // Space - O(H) where H is the height of the tree
+
+            int sumOfDepths = 0;
+            Stack<KeyValuePair<BinaryTree, int>> stack = new Stack<KeyValuePair<BinaryTree,int>>();
+            stack.Push(new KeyValuePair<BinaryTree, int>(tree, 0));
+            
+            while(stack.Count > 0)
+            {
+                var nodeInfo = stack.Pop();
+                var depth = nodeInfo.Value;
+
+                BinaryTree node = nodeInfo.Key;
+
+                if (node == null)
+                {
+                    continue;
+                }
+
+                sumOfDepths += depth;
+                stack.Push(new KeyValuePair<BinaryTree, int>(node.left, depth + 1));
+                stack.Push(new KeyValuePair<BinaryTree, int>(node.right, depth + 1));
+            }
+
+            return sumOfDepths;
+
+        }
+
+
+        public static BinaryTree CreateTree()
+        {
+            BinaryTree output = new BinaryTree(10);
+            output.left = new BinaryTree(5) { left = new BinaryTree(3), right = new BinaryTree(4) };
+            output.right = new BinaryTree(22) { left = new BinaryTree(20), right = new BinaryTree(29) };
+            return output;
+        }
+    }
+}
