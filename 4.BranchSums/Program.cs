727e6194d8e18c9681cac8a7225e6d915cd8e117
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-02 10:40:50 +0000

    Question 4 Complete

--- 4.BranchSums/Program.cs
+++ 4.BranchSums/Program.cs
@@ -32,7 +32,7 @@ namespace _4.BranchSums
 			Console.ReadLine();
         }
 
-		static List<int> BranchSums(BinaryTree root)
+		public static List<int> BranchSums(BinaryTree root)
         {
 			// Time = O(N) as we are traversing through every node but at each one performing a comstant time calcualtion. the constant time of O(1) is removed as it is insignigicant compared to O(N)
 			// Space = O(N) returning a list of N length
@@ -42,22 +42,22 @@ namespace _4.BranchSums
 			return sums;
         }
 		
-		static void CalculateBranchSums(BinaryTree node, int runningSum, List<int> BranchSums)
+		public static void CalculateBranchSums(BinaryTree node, int runningSum, List<int> BranchSums)
         {
             if (node == null)
             {
 				return;
             }
 
-			int newRunnungSum = runningSum + node.value;
+			int newRunningSum = runningSum + node.value;
 			if (node.left == null && node.right == null)
             {
-				BranchSums.Add(runningSum);
+				BranchSums.Add(newRunningSum);
 				runningSum = 0;
 				return;
             }
-			CalculateBranchSums(node.left, newRunnungSum, BranchSums);
-			CalculateBranchSums(node.right, newRunnungSum, BranchSums);
+			CalculateBranchSums(node.left, newRunningSum, BranchSums);
+			CalculateBranchSums(node.right, newRunningSum, BranchSums);
 
         }
 
