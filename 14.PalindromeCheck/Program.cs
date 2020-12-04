619573076bac3be59a825baa50490bf62bada502
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-04 12:36:38 +0000

    Question 14 - PalindromCheck - 2 solutions

---
+++ 14.PalindromeCheck/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _14.PalindromeCheck
+{
+    class Program
+    {
+        static string str = "abcdcba";
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"My method says : { IsPalindrome(str)} ");
+            Console.WriteLine($"Expert recursion method says : {IsPalindromeRecursion(str, 0)}");
+            Console.ReadKey();
+        }
+
+        public static bool IsPalindrome(string str)
+        {
+            // My solution before watching explanation
+
+            // Time - O(N)
+            // Space O(1)
+
+            int leftPointer = 0;
+            int rightPointer = str.Length-1;
+
+            while (leftPointer < rightPointer)
+            {
+                if (str[leftPointer] != str[rightPointer])
+                {
+                    return false;
+                }
+                else
+                {
+                    leftPointer++;
+                    rightPointer--;
+                }
+            }
+            return true; 
+        }
+
+        public static bool IsPalindromeRecursion(string str, int i)
+        {
+            // Time - O(N)
+            // Space - O(N)
+
+            int j = str.Length - 1 - i;
+            return i >= j ? true : str[i] == str[j] && IsPalindromeRecursion(str, i + 1);
+        }
+    }
+}
