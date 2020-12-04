3f9777296eb0ed58b0fd587e8a479a330c1577dd
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-04 21:24:16 +0000

    Question 16. - RunLengthEncoding Complete - 1 solution

---
+++ 16.RunLengthEncoding/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16.RunLengthEncoding
+{
+    class Program
+    {
+        static string testString = "AAAAAAAAAAAAABBCCCCDD";
+        //static string testString = "Aa";
+
+        static void Main(string[] args)
+        {
+            string result = RunLengthEncoding(testString);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static public string RunLengthEncoding(string str)
+        {
+            StringBuilder encodedChars = new StringBuilder();
+            int currentRun = 1;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                char currentChar = str[i];
+                char previousChar = str[i - 1];
+
+                if(currentChar != previousChar || currentRun == 9)
+                {
+                    encodedChars.Append(currentRun.ToString());
+                    encodedChars.Append(previousChar);
+                    currentRun = 0;
+                }
+                currentRun++;
+            }
+            encodedChars.Append(currentRun.ToString());
+            encodedChars.Append(str[str.Length - 1]);
+            return encodedChars.ToString();
+        }
+
+        
+    }
+}
