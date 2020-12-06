5926e20a7de14e960ab97f9ec52a211f3b6e85bf
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-06 16:42:07 +0000

    Question 24 - BinarySearchTreeConstruction - Not complete

---
+++ 24.BinarySearchTreeConstruction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _24.BinarySearchTreeConstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
