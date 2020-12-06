54d35aec70581e9b66b45be09a9fc503ac8e6250
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-06 22:53:41 +0000

    Question 25 - ValidateBinarySearchTree Complete - 1 solution

---
+++ 25.ValidateBinarySearchTree/Program.cs
@@ -0,0 +1,42 @@
+﻿using BinaryTreeHelper;
+using System;
+
+namespace _25.ValidateBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = BinaryTree.CreateTree();
+
+            // Time - O(N) where n is the number is nodes, because we are traversing every node
+            // Space - O(N) where N is the depth ( the length of the longest branch) because recursive calls are on the call stack
+
+            bool test = ValidateTree(tree);
+        }
+
+        public static bool ValidateTree(BinaryTree tree)
+        {
+
+            return validateBinaryTreeHelper(tree, int.MinValue, int.MaxValue);
+        }
+
+        public static bool validateBinaryTreeHelper(BinaryTree tree, int minValue, int maxValue)
+        {
+            if(tree == null)
+            {
+                // if we hit a leaf then the tree is valid
+                return true;
+            }
+            if(tree.value < minValue || tree.value >= maxValue)
+            {
+                return false;
+            }
+
+            bool leftIsValid = validateBinaryTreeHelper(tree.left, minValue, tree.value); // left node - max value of left node is current nodes value
+            return leftIsValid && validateBinaryTreeHelper(tree.right, tree.value, maxValue);
+
+
+        }
+    }
+}
