c0ec5d3c0f0a415ee567849c7b8cfa605a1edc33
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-14 12:07:34 +0000

    Question 30 - Binary Tree Diameter - 1 solution

---
+++ 30.BinaryTreeDiameter/Program.cs
@@ -0,0 +1,47 @@
+﻿using BinaryTreeHelper;
+using System;
+
+namespace _30.BinaryTreeDiameter
+{
+
+    public class TreeInfo
+    {
+        public int Diameter { get; set; }
+        public int Height { get; set; }
+
+        public TreeInfo(int diameter, int height)
+        {
+            this.Diameter = diameter;
+            this.Height = height;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Diameter is the lenght of the longest path in the tree
+            BinaryTree tree = BinaryTree.CreateTree();
+            int result = GetTreeInfo(tree).Diameter;
+        }
+
+        public static TreeInfo GetTreeInfo(BinaryTree node)
+        {
+            // Time & Space - O(N)
+
+            if(node == null)
+            {
+                return new TreeInfo(0, 0);
+            }
+
+            TreeInfo leftTreeInfo = GetTreeInfo(node.left);
+            TreeInfo rightTreeInfo = GetTreeInfo(node.right);
+
+            int longestPathThroughRoot = leftTreeInfo.Height + rightTreeInfo.Height;
+            int maxDiameterSoFar = Math.Max(leftTreeInfo.Diameter, rightTreeInfo.Diameter);
+            int currentDiameter = Math.Max(longestPathThroughRoot, maxDiameterSoFar);
+            int currentHeight = 1 + Math.Max(leftTreeInfo.Height, rightTreeInfo.Height);
+
+            return new TreeInfo(currentDiameter, currentHeight);
+        }
+    }
+}
