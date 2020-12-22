eecb5e5d5c8649a15ef09186bb271e12929f32bf
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-22 13:00:36 +0000

    Question 32 - LevenshteinDistance complete - 1 solution

--- 32.LevenshteinDistance/Program.cs
+++ 32.LevenshteinDistance/Program.cs
@@ -6,8 +6,10 @@ namespace _32.LevenshteinDistance
     {
         static void Main(string[] args)
         {
-			LevenshteinDistance("gumbo", "gambol");			
-        }
+			//LevenshteinDistance("gumbo", "gambol");
+			LevenshteinDistanceExpertSolution("gumbo", "gambol");
+
+		}
 
 		public static int LevenshteinDistance(string str1, string str2)
 		{
@@ -46,7 +48,45 @@ namespace _32.LevenshteinDistance
 			return changes;
 		}
 
-		
-		
+		public static int LevenshteinDistanceExpertSolution(string str1, string str2)
+        {
+			// Time O(nm) where is is the lenght on str1 and m is the length of str2
+			// Space O(nm)
+
+			// Still not 100% on this need to revisit
+
+			int[,] edits = new int[str2.Length + 1, str1.Length + 1];
+            for (int i = 0; i < str2.Length + 1; i++)
+            {
+                for (int j = 0; j < str1.Length + 1; j++)
+                {
+					edits[i, j] = j;
+                }
+				edits[i, 0] = i;
+            }
+
+            for (int i = 1; i < str2.Length + 1; i++)
+            {
+                for (int j = 1; j < str1.Length + 1; j++)
+                {
+					if(str2[i -1] == str1[j - 1])
+                    {
+						edits[i, j] = edits[i - 1, j - 1];
+                    }
+                    else
+                    {
+						edits[i, j] = 1 + Math.Min(edits[i - 1, j - 1], 
+							Math.Min(edits[i - 1, j], 
+							edits[i, j - 1]));
+                    }
+                }
+            }
+            
+
+            
+			return edits[str2.Length, str1.Length];
+        }
+
+
 	}
 }
