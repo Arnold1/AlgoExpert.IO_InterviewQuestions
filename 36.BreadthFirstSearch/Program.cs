c41fe6fa799bcb67e35706f905dc213dcd9dc788
Mike Hulme <mikehulme1983@googlemail.com>
2020-12-30 21:38:25 +0000

    Question 36 - BreadthFirstSearch complete - 1 solution

---
+++ 36.BreadthFirstSearch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _36.BreadthFirstSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+	public class Node
+	{
+		public string name;
+		public List<Node> children = new List<Node>();
+
+		public Node(string name)
+		{
+			this.name = name;
+		}
+
+		public List<string> BreadthFirstSearch(List<string> array)
+		{
+			// Time O(v + e)
+			// Space O(v)
+
+			Queue<Node> nodes = new Queue<Node>();
+			nodes.Enqueue(this);
+
+			while (nodes.Count > 0)
+            {
+				Node cuurent = nodes.Dequeue();
+				array.Add(cuurent.name);
+				cuurent.children.ForEach(n => nodes.Enqueue(n));
+            }
+			return array;
+		}
+
+		public Node AddChild(string name)
+		{
+			Node child = new Node(name);
+			children.Add(child);
+			return this;
+		}
+	}
+}
+
+
