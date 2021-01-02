2978f76e552f6ad62694dc1b8b33fe7ddadac3ee
Mike Hulme <mikehulme1983@googlemail.com>
2021-01-02 13:04:02 +0000

    Question 38 - Youngest Common Ancestor complete - 1 solution

---
+++ 38.YoungestCommonAncestor/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _38.YoungestCommonAncestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Time O(D) where D is the depth
+            // Space O(1)
+
+            // Code works but have not created the ancestaral tree to test it here. Works on Algoexpert.
+            
+        }
+        public static AncestralTree GetYoungestCommonAncestor(AncestralTree topAncestor, AncestralTree descendantOne, AncestralTree descendantTwo)
+        {
+            int depthOne = GetDescendantDepth(descendantOne, topAncestor);
+            int depthTwo = GetDescendantDepth(descendantTwo, topAncestor);
+
+            if(depthOne > depthTwo)
+            {
+                return BackTrackAncestralTree(descendantOne, descendantTwo, depthOne - depthTwo);
+            }
+            else
+            {
+                return BackTrackAncestralTree(descendantTwo, descendantOne, depthTwo - depthOne);
+            }
+        }
+
+        public static AncestralTree BackTrackAncestralTree(AncestralTree lowerDescendant, AncestralTree higherDescendant, int diff)
+        {
+            while(diff > 0)
+            {
+                lowerDescendant = lowerDescendant.ancestor;
+                diff--;
+            }
+
+            while(lowerDescendant != higherDescendant)
+            {
+                lowerDescendant = lowerDescendant.ancestor;
+                higherDescendant = higherDescendant.ancestor;
+            }
+
+            return lowerDescendant;
+        }
+
+        public static int GetDescendantDepth(AncestralTree descendant, AncestralTree topAncestor)
+        {
+            int depth = 0;
+            while(descendant != topAncestor)
+            {
+                depth++;
+                descendant = descendant.ancestor;
+            }
+            return depth;
+        }
+     }
+
+    public class AncestralTree
+    {
+        public char name;
+        public AncestralTree ancestor;
+
+        public AncestralTree(char name)
+        {
+            this.name = name;
+            this.ancestor = null;
+        }
+
+
+    }
+}
