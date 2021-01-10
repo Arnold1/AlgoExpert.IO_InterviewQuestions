445fc4b8aec65a9bf532a342579149598f5248cd
Mike Hulme <mikehulme1983@googlemail.com>
2021-01-10 22:19:05 +0000

    Question 40 - Permutations complete - 1 Solution

---
+++ 40.Permutations/List.cs
@@ -0,0 +1,6 @@
+﻿namespace _40.Permutations
+{
+    public class List<T1, T2>
+    {
+    }
+}
---
+++ 40.Permutations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _40.Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Time - O(n*n!)
+            // Space - O(n*n!)
+            List<int> array = new List<int> { 1, 2, 3, };
+            List<List<int>> permutations = new List<List<int>>();
+            GetPermuations(0, array, permutations);
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void GetPermuations(int i, List<int> array, List<List<int>> permutations)
+        {
+            if(i == array.Count - 1)
+            {
+                permutations.Add(new List<int>(array));
+            }
+            else
+            {
+                for (int j = i; j < array.Count; j++)
+                {
+                    Swap(array, i, j);
+                    GetPermuations(i + 1, array, permutations);
+                    Swap(array, i, j);
+                }
+            }
+        }
+        public static void Swap(List<int> array, int i, int j)
+        {
+            int tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
+}
